75f1178157b84e64799817485e392963d058d2d4
enzost4 <enzostrindade16@outlook.com>
2024-09-26 12:41:13 -0300

    verifying and bentchmarking

--- vec_add.cu
+++ vec_add.cu
@@ -3,6 +3,15 @@
 #include <time.h>
 #include <chrono>
 
+#define CUDA_CALL(f)                                                                     \
+    {                                                                                    \
+        cudaError_t err = f;                                                             \
+        if(err != cudaSuccess){                                                           \
+            fprintf(stderr, "%s\n at %s:%d\n", cudaGetErrorName(err), __FILE__, __LINE__);\
+            exit(1);\
+        }\
+    }\
+
 struct Vec{
     float * data;
     size_t n;
@@ -12,15 +21,16 @@ float get_rand(){
     return float(rand()) / RAND_MAX;
 };
 
-void vec_add_cpu(float * a, float * b, float * c, size_t n){
-    for(int i = 0; i < n; i++){
-        c[i] = a[i] + b[i];
+void vec_add_cpu(Vec * a, Vec * b, Vec * c){
+    for(int i = 0; i < a->n; i++){
+        c->data[i] = a->data[i] + b->data[i];
     }
 };
 
 void vec_init(Vec * v, size_t n){
     v->data = (float *)malloc(sizeof(float) * n);
     v->n = n;
+
     if(!v->data){
         fprintf(stderr, "failed to allocate vector\n");
         exit(1);
@@ -57,16 +67,7 @@ void vec_print(const Vec * v){
     printf("]\n");
 };
 
-#define N 1024
-
-#define CUDA_CALL(f)                                                                     \
-    {                                                                                    \
-        cudaError_t err = f;                                                             \
-        if(err != cudaSuccess){                                                           \
-            fprintf(stderr, "%s\n at %s:%d\n", cudaGetErrorName(err), __FILE__, __LINE__);\
-            exit(1);\
-        }\
-    }\
+#define N (1000000 * 20)
 
 __global__ void vec_add_gpu(float * a, float * b, float * c, size_t n){
     int num_threads = blockDim.x * blockDim.y * blockDim.z;
@@ -81,6 +82,50 @@ double get_elapsed(struct timespec start, struct timespec end) {
     return (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / 1e9;
 }
 
+bool verify(Vec * cpu_res, float * gpu_res){
+    float eps = 1e-5;
+    for(int i = 0; i < cpu_res->n; i++){
+        if(fabs(cpu_res->data[i] - gpu_res[i]) > eps){
+            return false;
+        }
+    }
+    return true;
+};
+
+double benchmark_cpu(Vec * a, Vec * b, Vec * c, int num_runs){
+    timespec start, end;
+    double cpu_elapsed_avg = 0.0f;
+
+    for(int i = 0; i < num_runs; i++){
+        clock_gettime(CLOCK_MONOTONIC, &start);
+        vec_add_cpu(a, b, c);
+        clock_gettime(CLOCK_MONOTONIC, &end);
+        double elapsed = get_elapsed(start, end);
+        cpu_elapsed_avg += elapsed;
+    }
+    cpu_elapsed_avg /= num_runs;
+
+    return cpu_elapsed_avg;
+};
+
+double benchmark_gpu(float * d_a, float * d_b, float * d_c, int num_runs){
+    size_t block_size = 256;
+    size_t num_blocks = (N + block_size  -1) / block_size;
+    timespec start, end;
+    double avg_elapsed;
+
+    for(int i = 0; i < num_runs; i++){
+        clock_gettime(CLOCK_MONOTONIC, &start);
+        vec_add_gpu<<<num_blocks, block_size>>>(d_a, d_b, d_c, N);
+        CUDA_CALL(cudaDeviceSynchronize());
+        clock_gettime(CLOCK_MONOTONIC, &end);
+        double elapsed = get_elapsed(start, end);
+        avg_elapsed += elapsed;
+    }
+    avg_elapsed /= num_runs;
+    return avg_elapsed;
+};
+
 int main(){
     srand(time(0));
     Vec a, b, c;
@@ -88,24 +133,7 @@ int main(){
     vec_init_rand(&a, N);
     vec_init_rand(&b, N);
     vec_init_zeros(&c, N);
-
-    //vec_print(&a);
-    //vec_print(&b);
-
-    timespec start, end;
-    double cpu_elapsed_avg = 0.0f;
-
-    for(int i = 0; i < 20; i++){
-        clock_gettime(CLOCK_MONOTONIC, &start);
-        vec_add_cpu(a.data, b.data, c.data, c.n);
-        clock_gettime(CLOCK_MONOTONIC, &end);
-        double elapsed = get_elapsed(start, end);
-        cpu_elapsed_avg += elapsed;
-    }
-
-    printf("elapsed time cpu: %lf\n", cpu_elapsed_avg);
-    
-    //vec_print(&c);
+    vec_add_cpu(&a, &b, &c);
 
     float * d_a, * d_b, * d_c;
 
@@ -127,7 +155,15 @@ int main(){
     Vec res;
     vec_init_zeros(&res, N);
     CUDA_CALL(cudaMemcpy(res.data, d_c, N * sizeof(float), cudaMemcpyDeviceToHost));
-    //vec_print(&res);
+
+    printf("Success: %d\n", verify(&c, res.data));
+    double cpu_elapsed_avg = benchmark_cpu(&a, &b, &c, 10);
+    double gpu_elapsed_avg = benchmark_gpu(d_a, d_b, d_c, 10);
+
+    printf("elapsed time cpu: %lf\n", cpu_elapsed_avg);
+    printf("elapsed time gpu: %lf\n", gpu_elapsed_avg);
+    printf("gpu is %f times faster than cpu\n", cpu_elapsed_avg / gpu_elapsed_avg);
+
 
     CUDA_CALL(cudaFree(d_a));
     CUDA_CALL(cudaFree(d_b));
